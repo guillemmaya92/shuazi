9013fb54cdd73bc8209a03b8d1442de160927bdc
guillem.maya <guillem.maya@kingspan.com>
2026-06-10 09:51:43 +0200

    solve header

---
+++ icons/README.txt
@@ -0,0 +1,6 @@
+Para PWA completo, genera iconos PNG:
+icon-192.png (192x192)
+icon-512.png (512x512)
+
+Usa https://icon.kitchen o GIMP/Photoshop
+con fondo negro (#000) y letra S blanca.
--- sw.js
+++ sw.js
@@ -1,9 +1,13 @@
 
 const CACHE_NAME = 'shuazi-v1';
+const DATA_CACHE_NAME = 'shuazi-data-v1';
+
 const urlsToCache = [
   '/',
   '/index.html',
-  '/manifest.json'
+  '/manifest.json',
+  '/icons/icon-192.svg',
+  '/icons/icon-512.svg'
 ];
 
 self.addEventListener('install', event => {
@@ -19,7 +23,7 @@ self.addEventListener('activate', event => {
     caches.keys().then(cacheNames => {
       return Promise.all(
         cacheNames.map(cacheName => {
-          if (cacheName !== CACHE_NAME) {
+          if (cacheName !== CACHE_NAME && cacheName !== DATA_CACHE_NAME) {
             return caches.delete(cacheName);
           }
         })
@@ -30,6 +34,11 @@ self.addEventListener('activate', event => {
 });
 
 self.addEventListener('fetch', event => {
+  // Skip non-http requests
+  if (!event.request.url.startsWith('http')) {
+    return;
+  }
+
   event.respondWith(
     caches.match(event.request)
       .then(response => {
@@ -39,4 +48,42 @@ self.addEventListener('fetch', event => {
         return fetch(event.request);
       })
   );
+});
+
+// Handle messages from main thread for data persistence
+self.addEventListener('message', event => {
+  if (event.data.type === 'SAVE_DATA') {
+    const data = event.data.payload;
+    event.waitUntil(
+      caches.open(DATA_CACHE_NAME)
+        .then(cache => {
+          const request = new Request('data.json');
+          cache.put(request, new Response(JSON.stringify(data), {
+            headers: { 'Content-Type': 'application/json' }
+          }));
+        })
+    );
+  }
+
+  if (event.data.type === 'GET_DATA') {
+    event.waitUntil(
+      caches.open(DATA_CACHE_NAME)
+        .then(cache => cache.match('data.json'))
+        .then(response => {
+          if (response) {
+            return response.json().then(data => {
+              event.source.postMessage({
+                type: 'DATA_LOADED',
+                payload: data
+              });
+            });
+          } else {
+            event.source.postMessage({
+              type: 'DATA_LOADED',
+              payload: null
+            });
+          }
+        })
+    );
+  }
 });
